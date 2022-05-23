e8244062ae10c5c315dea48869b4d498c386aeda
Santino Beltran <s.beltran@alumno.um.edu.ar>
2022-05-23 20:01:25 -0300

    TP 7 - Ej 4 hecho

---
+++ PycharmProjects/Computacion/TP 7/Ej_4.py
@@ -0,0 +1,5 @@
+def bingo(nums):
+    if 2 and 7 and 9 and 14 and 15 in nums:
+        return "WIN"
+    else:
+        return "LOSE"
---
+++ PycharmProjects/Computacion/TP 7/Ej_4_Test.py
@@ -0,0 +1,15 @@
+import unittest
+from Ej_4 import *
+
+class Testing(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), "LOSE")
+
+    def test_2(self):
+        self.assertEqual(bingo([20, 12, 23, 14, 6, 22, 12, 17, 2, 26]), "LOSE")
+
+    def test_3(self):
+        self.assertEqual(bingo([1, 2, 3, 7, 5, 14, 7, 15, 9, 10]), "WIN")
+
+    def test_4(self):
+        self.assertEqual(bingo([5, 2, 13, 7, 5, 14, 17, 15, 9, 10]), "WIN")
